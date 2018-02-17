9e4b28b48443446eebdea973a08899ccfc669ad2
gpenmike <gpenmike@gmail.com>
2018-02-17 11:15:19 +0700

    001

---
+++ Assets/Rotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+	float a = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		a += 2 * Time.deltaTime;
+		this.transform.rotation = Quaternion.Euler (a,a*3,a*2);
+	}
+}
